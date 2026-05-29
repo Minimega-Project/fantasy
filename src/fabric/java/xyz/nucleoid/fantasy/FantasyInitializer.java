fe11649f7884236edbc0c92df646e6227bcf74ea
Jab125 <67534807+Jab125@users.noreply.github.com>
2026-05-29 20:12:00 +1000

    readd FantasyInitializer for bincompat reasons

---
+++ src/fabric/java/xyz/nucleoid/fantasy/FantasyInitializer.java
@@ -0,0 +1,12 @@
+package xyz.nucleoid.fantasy;
+
+import net.fabricmc.api.ModInitializer;
+
+/// @deprecated Only exists for binary compatibility
+@Deprecated(forRemoval = true)
+public final class FantasyInitializer implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
